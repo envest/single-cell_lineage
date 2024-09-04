061ac0eebf52ae1e8086a5782e4d68d7a4b94ac8
envest <stevenmasonfoltz@gmail.com>
2024-09-04 15:16:35 -0400

    Number environments

---
+++ envs/numbat_extra_installation_steps.R
@@ -0,0 +1,4 @@
+install.packages("numbat", repos='http://cran.us.r-project.org')
+install.packages("Seurat", repos='http://cran.us.r-project.org')
+install.packages("Signac", repos='http://cran.us.r-project.org')
+BiocManager::install("ggtree", force = TRUE, type = 'source')
